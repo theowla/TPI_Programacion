4b0dcbb61ff744c4a5381e9944bc744781609f35
faksho <faksho@proton.me>
2026-06-08 19:18:17 -0300

    probando tabulate

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import csv
+from tabulate import tabulate
 
-with open('datos/dataset.csv', mode='r', encoding='utf-8') as archivo:
-    lector = csv.DictReader(archivo)
-    for fila in lector:
-        print(fila['nombre']) 
+columnas = ('nombre','poblacion','superficie','continente')
+
+def mostrar_datos(datos: list[dict])
+
+def cargar_paises():
+    with open('datos/dataset.csv', mode='r', encoding='utf-8') as archivo:
+        return list(csv.DictReader(archivo,columnas))
+
+datos = cargar_paises()
+
+print(tabulate(datos,tablefmt='simple_outline'))
