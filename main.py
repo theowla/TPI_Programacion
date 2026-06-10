60a00ea0ae4d03b30e7ac61724a09c17c9f0b321
faksho <faksho@proton.me>
2026-06-10 18:36:34 -0300

    agregado estadisticas() y mas entradas al dataset

--- main.py
+++ main.py
@@ -103,8 +103,34 @@ def filtrar_pais():
     else:
         print("opcion invalida")
 
+def estadisticas():
+    ordenado = []
+    ordenado = sorted(datos[1:], key=lambda x: int(x["poblacion"]))
+
+    print(f"Menor población: {ordenado[0]['nombre']}\nMayor población: {ordenado[-1]['nombre']}")
+
+    conteo_continentes = {}
+    poblacion = 0
+    superficie = 0
+
+    for item in datos:
+        if item["poblacion"] == "poblacion":
+            continue
+        poblacion += int(item["poblacion"])
+        superficie += int(item["superficie"])
+        continente = item["continente"]
+        conteo_continentes[continente] = conteo_continentes.get(continente, 0) + 1
+
+    promedio_poblacion = poblacion / len(datos)
+    promedio_superficie = superficie / len(datos)
+    print(f"Promedio de población: {promedio_poblacion:.0f}")
+    print(f"Promedio de superficie: {promedio_superficie:.0f}")
+    for item in conteo_continentes:
+        print(f"{item} = {conteo_continentes[item]}")
+
+estadisticas()
 #actualizar_pais()
-mostrar_ordenado("s",False)
+#mostrar_ordenado("s",False)
 
 #agregar_entrada()
 #mostrar_datos(datos)
